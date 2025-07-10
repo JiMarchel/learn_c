8962c529101b816d6c23de2d8e9869975b96d602
JiMarchel <jmarchel100@gmail.com>
2025-07-10 16:55:31 +0700

    hello

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-main(){
-    printf("Hello ");
-    printf("World");
-    printf("\n");
+int main()
+{
+    int i;
+    for(i=0; i < 10; i++)
+    {
+        puts("Hello world");
+    }
+    return 0;
 }
